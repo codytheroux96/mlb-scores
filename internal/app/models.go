02f3f5648a4e6b8ebcb357b83a335fb13634bcc0
codytheroux96 <cody.theroux3@gmail.com>
2024-12-23 17:16:30 -0500

    making the models for team, game, and games

--- internal/app/models.go
+++ internal/app/models.go
@@ -1,2 +1,22 @@
 package app
 
+type Team struct {
+	Name   string
+	isHome bool
+}
+
+type Game struct {
+	HomeTeam    Team
+	AwayTeam    Team
+	HomeScore   int
+	AwayScore   int
+	HomePitcher string
+	AwayPitcher string
+	GameTime    string
+	GameStatus  string
+}
+
+type Games struct {
+	Date  string
+	Games []Game
+}
