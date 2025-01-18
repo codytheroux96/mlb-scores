313e92289bb13efb1e5031f7ea96e0abc90f9ed6
codytheroux96 <cody.theroux3@gmail.com>
2025-01-18 15:19:34 -0500

    made the case for yesterday

--- cmd/main.go
+++ cmd/main.go
@@ -24,6 +24,34 @@ func main() {
 			fmt.Println("Error fetching today's scores:", err)
 			os.Exit(1)
 		}
+
+		for _, game := range scores.Data {
+			parsedDate, err := time.Parse(time.RFC3339, game.Date)
+			if err != nil {
+				fmt.Println("Error parsing date:", err)
+				continue
+			}
+
+			formattedDate := parsedDate.Format("2006-01-02")
+			homeTeam := game.HomeTeam.DisplayName
+			awayTeam := game.AwayTeam.DisplayName
+			homeScore := game.HomeTeamData.Runs
+			awayScore := game.AwayTeamData.Runs
+			status := game.Status
+
+			fmt.Printf("Date: %s\nHome Team: %s\nAway Team: %s\nScore: %d - %d\nStatus: %s\n\n",
+				formattedDate, homeTeam, awayTeam, homeScore, awayScore, status)
+		}
+	case "yesterday":
+		date := time.Now().AddDate(0, 0, -1).Format("2006-01-02")
+		endDate := time.Now().Format("2006-01-02")
+
+		scores, err := api.GetScores(date, endDate)
+		if err != nil {
+			fmt.Println("Error fetching yesterday's scores:", err)
+			os.Exit(1)
+		}
+
 		for _, game := range scores.Data {
 			parsedDate, err := time.Parse(time.RFC3339, game.Date)
 			if err != nil {
