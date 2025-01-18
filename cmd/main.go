1d890691d5931a31df6c019b7a1e909d017aed37
codytheroux96 <cody.theroux3@gmail.com>
2025-01-18 16:34:42 -0500

    making the get todays scores function and calling it in main

--- cmd/main.go
+++ cmd/main.go
@@ -18,39 +18,15 @@ func main() {
 	case "today":
 		date := time.Now().Format("2006-01-02")
 		endDate := time.Now().AddDate(0, 0, 1).Format("2006-01-02")
-
-		scores, err := api.GetScores(date, endDate)
+		
+		scores, err := api.GetTodaysScores(date, endDate)
 		if err != nil {
 			fmt.Println("Error fetching today's scores:", err)
 			os.Exit(1)
 		}
 
-		for _, game := range scores.Data {
-			parsedDate, err := time.Parse(time.RFC3339, game.Date)
-			if err != nil {
-				fmt.Println("Error parsing date:", err)
-				continue
-			}
-
-			var winner string
-
-			if game.HomeTeamData.Runs > game.AwayTeamData.Runs {
-				winner = game.HomeTeam.DisplayName
-			} else if game.AwayTeamData.Runs < game.HomeTeamData.Runs {
-				winner = game.AwayTeam.DisplayName
-			} else {
-				winner = "Game is not over yet"
-			}
-
-			formattedDate := parsedDate.Format("2006-01-02")
-			homeTeam := game.HomeTeam.DisplayName
-			awayTeam := game.AwayTeam.DisplayName
-			awayScore := game.AwayTeamData.Runs
-			homeScore := game.HomeTeamData.Runs
-			status := game.Status
-
-			fmt.Printf("Date: %s\nHome Team: %s\nAway Team: %s\nScore: %d - %d\nStatus: %s\nWinner: %s\n\n",
-				formattedDate, homeTeam, awayTeam, awayScore, homeScore, status, winner)
+		for _, score := range scores {
+			fmt.Println(score)
 		}
 	case "yesterday":
 		date := time.Now().AddDate(0, 0, -1).Format("2006-01-02")
@@ -80,8 +56,8 @@ func main() {
 			}
 
 			formattedDate := parsedDate.Format("2006-01-02")
-			homeTeam := game.HomeTeam.DisplayName
 			awayTeam := game.AwayTeam.DisplayName
+			homeTeam := game.HomeTeam.DisplayName
 			homeScore := game.HomeTeamData.Runs
 			awayScore := game.AwayTeamData.Runs
 			status := game.Status
